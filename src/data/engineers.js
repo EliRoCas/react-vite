9d496188f44629f2aec4f7e82d49d8222f83c2fd
Eli Rosero <elir_94@hotmail.com>
2024-05-01 11:18:05 -0500

    Css engineers card config

--- src/data/engineers.js
+++ src/data/engineers.js
@@ -3,37 +3,37 @@ const engineers = [
         nname: "Edwin Arias",
         ndegree: "BackEnd Developer",
         ndescription: "Construye y mantiene la lógica y la funcionalidad de los servidores y bases de datos que respaldan las aplicaciones.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Sql_database_shortcut_icon.png"
+        image: "https://programacion.net/files/article/20151111051149_tipos-de-programacion.jpg"
     },
     {
         nname: "Dante Arias",
         ndegree: "FrontEnd Developer",
         ndescription: "Diseña y desarrolla la interfaz de usuario y la experiencia del usuario en aplicaciones web y móviles.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/375px-React.svg.png"
+        image: "https://cms.rootstack.com/sites/default/files/inline-images/Captura%20de%20pantalla%202023-08-02%20a%20la%28s%29%2013.20.50.png"
     },
     {
         nname: "Triz Rosero",
         ndegree: "DevOps Engineer",
         ndescription: "Encargada de la automatización, integración y despliegue continuo de aplicaciones y servicios.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/300px-PHP-logo.svg.png"
+        image: "https://trbl-services.eu/wp-content/uploads/2021/03/dev1.png"
     },
     {
         nname: "Pepito Pérez",
         ndegree: "Data Engineer",
         ndescription: "Experto en el diseño y gestión de infraestructuras para almacenamiento y procesamiento de datos.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Sql_database_shortcut_icon.png"
+        image: "https://kinsta.com/es/wp-content/uploads/sites/8/2022/10/types-of-developers-1024x512.jpg"
     },
     {
         nname: "Cindy Gutierrez",
         ndegree: "Security Engineer",
         ndescription: "Especializado en la protección y seguridad de sistemas, redes y datos contra amenazas cibernéticas.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/480px-C_Sharp_Logo_2023.svg.png"
+        image: "https://www.esic.edu/sites/default/files/styles/full/public/rethink/7e2a0f50-tipos-de-seguridad-informatica.jpg?itok=gBisG41U"
     },
     {
         nname: "Mateo Rosero",
         ndegree: "Full-stack Developer",
         ndescription: "Experto en la creación de páginas web, microservicios y desarrollo de aplicaciones",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Angularjsoldicon.png/375px-Angularjsoldicon.png"
+        image: "https://i.ytimg.com/vi/_Y7Ql3ZYFF8/maxresdefault.jpg"
     }
 ]
 
